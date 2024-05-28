f48ce0c27a5525037895c0b053cc84742a66301d
Aws_killer <telegramsuper6@gmail.com>
2024-05-28 14:16:19 +0300

    playwrite this bitch

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 # Use the official Python image as the base image
-FROM python:3.9-slim
+FROM mcr.microsoft.com/playwright:focal
+
+RUN apt-get update && apt-get install -y python3-pip
 
 # Set the working directory within the container
 WORKDIR /srv
--- Pi.py
+++ Pi.py
@@ -1,9 +1,9 @@
-import aiohttp
 import asyncio
-import enum
-import requests
 import os
 from functools import cache
+import enum
+import requests
+from playwright.async_api import async_playwright, Page, Browser, BrowserContext
 
 
 class VoiceType(enum.Enum):
@@ -31,7 +31,6 @@ class PiAIClient:
         self.user_agent = (
             "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0"
         )
-        # self.cookie = "__Host-session=63EQahvTpHuoFSkEW75hC; __cf_bm=CDGicP5OErYjDI85UmQSRKlppJLlbcgCXlWcODoIQAI-1716296320-1.0.1.1-4Rm5_wdxupmrDWgddOQjEV01TMFC4UJ479GRIAKKGHNgXu3N8ZkASEZXGwCWaRyUYazsUaLMALk.4frWWJzHQ"
         self.cookie = None
         self.headers = {
             "User-Agent": self.user_agent,
@@ -52,42 +51,49 @@ class PiAIClient:
         }
 
     @cache
-    async def get_cookie(self) -> str:
+    async def get_cookie(self, context: BrowserContext) -> str:
         headers = self.headers.copy()
-        async with aiohttp.ClientSession() as session:
-            async with session.post(
-                f"{self.base_url}/start", headers=headers, json={}
-            ) as response:
-                self.cookie = response.headers["Set-Cookie"]
-                return self.cookie
+        page = await context.new_page()
+        response = await page.request.post(
+            f"{self.base_url}/start", headers=headers, data={}
+        )
+        self.cookie = response.headers["set-cookie"]
+        await page.close()
+        return self.cookie
 
     async def make_request(
-        self, endpoint: str, headers: dict, json: dict = None, method: str = "POST"
+        self,
+        context: BrowserContext,
+        endpoint: str,
+        headers: dict,
+        json: dict = None,
+        method: str = "POST",
     ):
-        async with aiohttp.ClientSession() as session:
-            if method == "POST":
-                async with session.post(
-                    endpoint, headers=headers, json=json
-                ) as response:
-                    return await response.text()
-            elif method == "GET":
-                async with session.get(endpoint, headers=headers) as response:
-                    return response
+        page = await context.new_page()
+        if method == "POST":
+            response = await page.request.post(endpoint, headers=headers, data=json)
+        elif method == "GET":
+            response = await page.request.get(endpoint, headers=headers)
+        await page.close()
+        return await response.text()
 
-    async def get_response(self, input_text) -> tuple[list[str], list[str]]:
+    async def get_response(
+        self, context: BrowserContext, input_text
+    ) -> tuple[list[str], list[str]]:
         if self.cookie is None:
-            self.cookie = await self.get_cookie()
+            self.cookie = await self.get_cookie(context)
 
         headers = self.headers.copy()
         headers["Cookie"] = self.cookie
 
         data = {"text": input_text}
-        response_text = await self.make_request(self.base_url, headers, json=data)
+        response_text = await self.make_request(
+            context, self.base_url, headers, json=data
+        )
 
         response_lines = response_text.split("\n")
         response_texts = []
         response_sids = []
-        print(response_lines)
         for line in response_lines:
             if line.startswith('data: {"text":"'):
                 start = len('data: {"text":')
@@ -104,26 +110,13 @@ class PiAIClient:
         return response_texts, response_sids
 
     async def speak_response(
-        self, message_sid: str, voice: VoiceType = VoiceType.voice4
+        self,
+        context: BrowserContext,
+        message_sid: str,
+        voice: VoiceType = VoiceType.voice4,
     ) -> None:
         if self.cookie is None:
-            self.cookie = await self.get_cookie()
-
-        headers = self.headers.copy()
-        headers.update(
-            {
-                "Host": "pi.ai",
-                "Accept": "audio/webm,audio/ogg,audio/wav,audio/*;q=0.9,application/ogg;q=0.7,video/*;q=0.6,*/*;q=0.5",
-                "Accept-Language": "en-US,en;q=0.9",
-                "Range": "bytes=0-",
-                "Sec-Fetch-Dest": "audio",
-                "Sec-Fetch-Mode": "no-cors",
-                "Sec-Fetch-Site": "same-origin",
-                "Sec-CH-UA": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-                "Sec-CH-UA-Mobile": "?0",
-                "Sec-CH-UA-Platform": '"Windows"',
-            }
-        )
+            self.cookie = await self.get_cookie(context)
 
         headers = {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0",
@@ -132,7 +125,7 @@ class PiAIClient:
             "Range": "bytes=0-",
             "Connection": "keep-alive",
             "Referer": "https://pi.ai/talk",
-            # "Cookie": cookie,
+            "Cookie": self.cookie,
             "Sec-Fetch-Dest": "audio",
             "Sec-Fetch-Mode": "no-cors",
             "Sec-Fetch-Site": "same-origin",
@@ -141,25 +134,23 @@ class PiAIClient:
             "Accept-Encoding": "identity",
             "TE": "trailers",
         }
-        headers["Cookie"] = self.cookie
-        print(headers)
         endpoint = f"{self.base_url}/voice?mode=eager&voice={voice.value}&messageSid={message_sid}"
-        async with aiohttp.ClientSession() as session:
-            async with session.get(endpoint, headers=headers) as response:
-                print(response.status)
-                if response.status == 200:
-                    with open("speak.wav", "wb") as file:
-                        async for chunk in response.content.iter_chunked(128):
-                            file.write(chunk)
+        page = await context.new_page()
+        response = await page.request.get(endpoint, headers=headers)
+        if response.status == 200:
+            with open("speak.wav", "wb") as file:
+                async for chunk in response.body():
+                    file.write(chunk)
 
-                    # Run command vlc to play the audio file
-                    os.system("vlc speak.wav --intf dummy --play-and-exit")
-                else:
-                    print("Error: Unable to retrieve audio.")
+            # Run command vlc to play the audio file
+            os.system("vlc speak.wav --intf dummy --play-and-exit")
+        else:
+            print("Error: Unable to retrieve audio.")
+        await page.close()
 
-    async def trigger_event(self):
+    async def trigger_event(self, context: BrowserContext):
         if self.cookie is None:
-            self.cookie = await self.get_cookie()
+            self.cookie = await self.get_cookie(context)
 
         headers = {
             "accept": "*/*",
@@ -188,56 +179,23 @@ class PiAIClient:
 
         endpoint = "https://pi.ai/proxy/api/event"
         response_text = await self.make_request(
-            endpoint, headers, json=data, method="POST"
+            context, endpoint, headers, json=data, method="POST"
         )
         print(response_text)
 
 
-def speak_response(
-    message_sid: str, voice: VoiceType = VoiceType.voice4, cookie: str = None
-) -> None:
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0",
-        "Accept": "audio/webm,audio/ogg,audio/wav,audio/*;q=0.9,application/ogg;q=0.7,video/*;q=0.6,*/*;q=0.5",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Range": "bytes=0-",
-        "Connection": "keep-alive",
-        "Referer": "https://pi.ai/talk",
-        "Cookie": cookie,
-        "Sec-Fetch-Dest": "audio",
-        "Sec-Fetch-Mode": "no-cors",
-        "Sec-Fetch-Site": "same-origin",
-        "DNT": "1",
-        "Sec-GPC": "1",
-        "Accept-Encoding": "identity",
-        "TE": "trailers",
-    }
-    print(
-        f"https://pi.ai/api/chat/voice?messageSid={message_sid}&voice=qdpi&mode=eager"
-    )
-    response = requests.get(
-        f"https://heypi.com/api/chat/voice?messageSid={message_sid}&voice=qdpi&mode=eager",
-        headers=headers,
-        stream=True,
-    )
-    print(response.headers)
-    # Ensure the request was successful
-
-    if response.status_code == 200:
-        # Open a .wav file in write-binary mode
-        with open("asdasd.wav", "wb") as file:
-            # Write the contents of the response to the file
-            for chunk in response.iter_content(chunk_size=128):
-                file.write(chunk)
-
-    else:
-        print("Error: Unable to retrieve audio.")
-
-
 async def handleSpeak(text):
-    client = PiAIClient()
-    response_texts, response_sids = await client.get_response(text)
-    print(response_texts, response_sids)
-    await client.trigger_event()
-    if response_sids:
-        speak_response(response_sids[0], cookie=client.cookie)
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        context = await browser.new_context()
+        client = PiAIClient()
+        response_texts, response_sids = await client.get_response(context, text)
+        print(response_texts, response_sids)
+        await client.trigger_event(context)
+        if response_sids:
+            await client.speak_response(context, response_sids[0])
+        await browser.close()
+
+
+# # To run the handleSpeak function:
+# asyncio.run(handleSpeak("Hello, how are you?"))
