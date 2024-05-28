f68ec4821cc331b3f3f492802d579f5ec442b240
Aws_killer <telegramsuper6@gmail.com>
2024-05-28 13:52:49 +0300

    no huggingface

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 import aiohttp
-from HuggingChat import chatbot as huggingChat
+
+# from HuggingChat import chatbot as huggingChat
 import json
 from pydantic import BaseModel
 from Pi import handleSpeak
@@ -17,13 +18,13 @@ class Speaker(BaseModel):
 app = FastAPI()
 
 
-@app.post("/generate")
-async def generate_message(request_body: Req):
-    prompt = request_body.prompt
+# @app.post("/generate")
+# async def generate_message(request_body: Req):
+#     prompt = request_body.prompt
 
-    temp = str(huggingChat.query(prompt))
-    temp = json.loads(temp.split("```json")[1].split("```")[0].strip())
-    return temp
+#     temp = str(huggingChat.query(prompt))
+#     temp = json.loads(temp.split("```json")[1].split("```")[0].strip())
+#     return temp
 
 
 @app.post("/speak")
